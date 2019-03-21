5aaeb8cbcc16f33cdc24efd031269420d83f5cdf
manu9812 <dyg9812@gmail.com>
2019-03-20 20:50:05 -0500

    agregando comentarios

--- Tetraminos/tetramino_L.js
+++ Tetraminos/tetramino_L.js
@@ -1,32 +1,65 @@
-class TetraminoL {
 
-  let  tablero[][];
+  /**
+   * [TetraminoL description]
+   */
+  class TetraminoL {
 
-  constructor() {
+      let  tablero[][];
+
+    /**
+     * [constructor description]
+     */
+      constructor() {
+
+      }
+
+
+    /**
+     * [startTetamine description]
+     * @return {[type]} [description]
+     */
+    startTetamine(){
+
+    }
+
+    /**
+     * [moveRight description]
+     * @return {[type]} [description]
+     */
+    moveRight (){
+
+    }
+
+    /**
+     * [moveLeft description]
+     * @return {[type]} [description]
+     */
+    moveLeft(){
+
+    }
+
+    /**
+     * [decline description]
+     * @return {[type]} [description]
+     */
+    decline(){
+
+    }
+
+    /**
+     * [turnRight description]
+     * @return {[type]} [description]
+     */
+    turnRight(){
+
+    }
+
+    /**
+     * [lastState description]
+     * @return {[type]} [description]
+     */
+    lastState(){
+
+    }
 
   }
-
-startTetamine(){
-
-}
-
-moveRight (){
-
-}
-
-moveLeft(){
-
-}
-
-decline(){
-
-}
-
-turnRight(){
-
-}
-
-lastState(){
-  
-}
-}
