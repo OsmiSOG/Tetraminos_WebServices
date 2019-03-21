7d665472565ab01b1b6ac22002abd918c1c7911e
manu9812 <dyg9812@gmail.com>
2019-03-20 20:53:16 -0500

    agregando metodos a la clase padre

--- Tetraminos/tetramino.js
+++ Tetraminos/tetramino.js
@@ -1,7 +1,32 @@
+
+/**
+ * [Tetramino description]
+ */
 class Tetramino {
+
+  let tablero;
   constructor() {
 
   }
+  /**
+   * [moveRight description]
+   * @return {[type]} [description]
+   */
+  moveRight(){
 
+  }
+  /**
+   * [moveLeft description]
+   * @return {[type]} [description]
+   */
+  moveLeft(){
 
+  }
+  /**
+   * [decline description]
+   * @return {[type]} [description]
+   */
+  decline(){
+
+  }
 }
